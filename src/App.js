d18e3d2a861ada4d33195221229014509877b70d
mistok <Mistreated@ukr.net>
2021-04-15 15:37:29 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import nicky from './img/nicky.jpg';
+import preloader from './img/preloader.svg';
 import './App.css';
+import React, {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let [isLoading, setIsLoading] = useState(true);
+    let [error, setError] = useState(false);
+
+    useEffect(()=> {
+            const isOdd = num => num % 2 ===0;
+            const getTwoNumbers =  () => {
+                const num1 = fetch('https://www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new')
+                    .then(result => result.text())
+                    .then( result => +result )
+                    .catch(error => console.log(error.message));
+                const num2 = fetch('https://www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new')
+                    .then(result => result.text())
+                    .then( result => +result )
+                    .catch(error => setError(error));
+                const nums = [num1, num2];
+                Promise.all(nums).then((nums) => {
+                    isOdd(nums[0]) && isOdd(nums[1]) ? setIsLoading(false) : getTwoNumbers() ;
+                })
+            };
+            getTwoNumbers();
+        },[]);
+
+    return (
+        <div className="App">
+          <header className="header">
+            Three pictures
+          </header>
+            <div className="images_container">
+                {isLoading ? <div className="image_container"><img className="preloader" src={preloader} alt="preloader"/></div> : <div className="image_container"><img className="target_image" src={nicky} alt="Cage"/></div> }
+                {isLoading ? <div className="image_container"><img className="preloader" src={preloader} alt="preloader"/></div> : <div className="image_container"><img className="target_image" src={nicky} alt="Cage"/></div> }
+                {isLoading ? <div className="image_container"><img className="preloader" src={preloader} alt="preloader"/></div> : <div className="image_container"><img className="target_image" src={nicky} alt="Cage"/></div> }
+            </div>
+            <div>
+                <p>{error ? error : ''}</p>
+            </div>
+        </div>
+    );
 }
 
 export default App;
